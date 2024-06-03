81e55fa6a0e92ca538987b3c70c7046956ed94e1
Upadhyay-Shweta <shwetaupadhyay462@gmail.com>
2024-06-03 09:43:43 +0530

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['https://fakestoreapi.com/products'],
+        domains: ['fakestoreapi.com'],
       },
 };
   
 export default nextConfig;
+
